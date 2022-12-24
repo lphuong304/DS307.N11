f597b1e45c84d0f6fdf856c7b2e2335f15f39b6f
LAN PHƯƠNG <19520227@gm.uit.edu.vn>
2022-12-24 11:38:04 +0700

    upload code but datasets

---
+++ traning/BERT.py
@@ -0,0 +1,103 @@
+import torch
+
+from keras.preprocessing.sequence import pad_sequences
+from sklearn.model_selection import train_test_split
+import preprocessor as p
+
+from transformers import XLMModel, BertTokenizer, BertForSequenceClassification
+from transformers import AdamW
+
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+
+from tqdm import tqdm, trange
+import pandas as pd
+import io
+import numpy as np
+import matplotlib.pyplot as plt
+from config import *
+from utils import *
+import os
+
+def training(model, epochs, train_dataloader, validation_dataloader, optimizer):
+
+    train_loss_set = []
+    est_val_accuracy = 0.90
+
+    for _ in trange(epochs, desc="Epoch"):
+        model.train()
+        tr_loss = 0
+        nb_tr_examples, nb_tr_steps = 0, 0
+    
+        for step, batch in enumerate(train_dataloader):
+            batch = tuple(t.to(device) for t in batch)
+            b_input_ids, b_input_mask, b_labels = batch
+            optimizer.zero_grad()
+            outputs = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask, labels=b_labels)
+            loss = outputs[0]
+            logits = outputs[1]
+            train_loss_set.append(loss.item())    
+            loss.backward()
+            optimizer.step()
+
+            tr_loss += loss.item()
+            nb_tr_examples += b_input_ids.size(0)
+            nb_tr_steps += 1
+
+        print("Train loss: {}".format(tr_loss/nb_tr_steps))
+
+        model.eval()
+
+        eval_loss, eval_accuracy = 0, 0
+        nb_eval_steps, nb_eval_examples = 0, 0
+
+        for batch in validation_dataloader:
+            batch = tuple(t.to(device) for t in batch)
+            b_input_ids, b_input_mask, b_labels = batch
+            with torch.no_grad():
+                output = model(b_input_ids, token_type_ids=None, attention_mask=b_input_mask)
+                logits = output[0]
+
+            logits = logits.detach().cpu().numpy()
+            label_ids = b_labels.to('cpu').numpy()
+
+            tmp_eval_accuracy = get_accuracy(logits, label_ids)
+
+            eval_accuracy += tmp_eval_accuracy
+            nb_eval_steps += 1
+
+    print("Validation Accuracy: {}".format(eval_accuracy/nb_eval_steps))
+    Validation_Accuracy = (eval_accuracy/nb_eval_steps)
+    if(Validation_Accuracy >= best_val_accuracy):
+        torch.save(model.state_dict(), CONFIG_SAVE_CHECKPOINTS+'models/BERT_best_model.pt')
+        best_val_accuracy = Validation_Accuracy
+        print('Model Saved')
+
+
+
+if __name__ == '__main__':
+    device = CONFIG_DEVICE
+    n_gpu = torch.cuda.device_count()
+    torch.cuda.get_device_name(0)
+
+    data_train = pd.read_csv(os.path.join(CONFIG_PATH_INPUT, 'train.csv'))
+    data_val = pd.read_csv(os.path.join(CONFIG_PATH_INPUT, 'val.csv'))
+    data_test = pd.read_csv(os.path.join(CONFIG_PATH_INPUT, 'test.csv'))
+
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
+    
+    model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2).to(CONFIG_DEVICE)
+
+    param_optimizer = list(model.named_parameters())
+    no_decay = ['bias', 'gamma', 'beta']
+    optimizer_grouped_parameters = [
+        {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)],
+        'weight_decay_rate': 0.01},
+        {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)],
+        'weight_decay_rate': 0.0}
+    ]
+    train_dataloader, validation_dataloader, test_dataloader = mydata_loader(data_train, data_val, data_test)
+
+    optimizer = AdamW(optimizer_grouped_parameters, lr=CONFIG_LR)
+    
+    training(model, CONFIG_EPOCH, train_dataloader, validation_dataloader, optimizer)
---
+++ utils.py
@@ -0,0 +1,89 @@
+import preprocessor as p
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem import 	WordNetLemmatizer
+from nltk.stem.porter import PorterStemmer
+stop_words = set(stopwords.words('english'))
+import os
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+import torch
+from config import *
+import numpy as np
+
+def check_and_make_dir(path):
+    if not os.exists(path):
+        os.makedirs(path)
+
+def preprocess(row):
+    text = row['tweet']
+    
+    text = p.clean(text)
+    tokenization = nltk.word_tokenize(text)     
+    tokenization = [w for w in tokenization if not w in stop_words]
+
+    return text
+
+def map_label(row):
+    return 0 if row['label']=='real' else 1
+
+def getAttention(sentence,tokenizer,maxlen,padding_type='max_length',attention_mask_flag=True):
+    encoded_dict = tokenizer.encode_plus(sentence, add_special_tokens=True, max_length=maxlen, truncation=True, padding=padding_type, return_attention_mask=attention_mask_flag)
+    return encoded_dict['input_ids'],encoded_dict['attention_mask']
+
+
+def getTokenizedText(sentenceList, tokenizer):
+    token_ids_list,attention_mask_list = [],[]
+    for sentence in sentenceList:
+        token_ids,attention_mask = getAttention(sentence,tokenizer,MAX_LEN)
+        token_ids_list.append(token_ids)
+        attention_mask_list.append(attention_mask)
+    return token_ids_list,attention_mask_list
+
+def get_accuracy(preds, labels):
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
+
+
+def mydata_loader(data_train, data_test, data_val, tokenizer):
+    wordnet = WordNetLemmatizer()
+    porter  = PorterStemmer()
+
+    data_train['tweet'] = data_train.apply(lambda x: preprocess(x, wordnet, porter), 1)
+    data_val['tweet'] = data_val.apply(lambda x: preprocess(x, wordnet, porter), 1)
+    data_test['tweet'] = data_test.apply(lambda x: preprocess(x, wordnet, porter), 1)
+
+    data_train['label_encoded'] = data_train.apply(lambda x: map_label(x), 1)
+    data_val['label_encoded'] = data_val.apply(lambda x: map_label(x), 1)
+
+    train_sentences = data_train.tweet.values
+    val_sentences = data_val.tweet.values
+    test_sentences = data_test.tweet.values
+
+    train_labels = data_val.label_encoded.values
+    val_labels = data_val.label_encoded.values
+
+    train_token_ids,train_attention_masks = torch.tensor(getTokenizedText(train_sentences,tokenizer))
+    val_token_ids,val_attention_masks = torch.tensor(getTokenizedText(val_sentences,tokenizer))
+    test_token_ids,test_attention_masks = torch.tensor(getTokenizedText(test_sentences,tokenizer))
+
+    train_labels = torch.tensor(train_labels)
+    val_labels = torch.tensor(val_labels)
+
+
+    train_data = TensorDataset(train_token_ids, train_attention_masks, train_labels)
+    train_sampler = RandomSampler(train_data)
+    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=CONFIG_BATCH_SIZE)
+
+    validation_data = TensorDataset(val_token_ids, val_attention_masks, val_labels)
+    validation_sampler = SequentialSampler(validation_data)
+    validation_dataloader = DataLoader(validation_data, sampler=validation_sampler, batch_size=CONFIG_BATCH_SIZE)
+
+    test_data = TensorDataset(test_token_ids, test_attention_masks)
+    test_sampler = SequentialSampler(test_data)
+    test_dataloader = DataLoader(test_data, sampler=test_sampler, batch_size=CONFIG_BATCH_SIZE)
+
+    return train_dataloader, validation_dataloader, test_dataloader
+
+
+
